cac7a05f473c1c1a6d0fb3239c119ec84db5f6ec
홍채민 <coals0329@cocen.com>
2022-03-22 16:44:34 +0900

    [minHong] spring-data-jpa uses paging 2 : countQuery optimization

--- src/main/java/com/example/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/com/example/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -3,12 +3,15 @@ package com.example.study.querydsl.repository;
 import com.example.study.querydsl.dto.MemberSearchCondition;
 import com.example.study.querydsl.dto.MemberTeamDTO;
 import com.example.study.querydsl.dto.QMemberTeamDTO;
+import com.example.study.querydsl.entity.Member;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
 
@@ -69,7 +72,7 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
     }
 
     @Override
-    public Page<MemberTeamDTO> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+    public Page searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
         List<MemberTeamDTO> results = queryFactory
                 .select(new QMemberTeamDTO(
                         member.id,
@@ -89,7 +92,8 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
-        long total = queryFactory
+
+        JPAQuery<Member> countQuery = queryFactory
                 .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
@@ -98,10 +102,14 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
                     teamNameEq(condition.getTeamName()),
                     ageGoe(condition.getAgeGoe()),
                     ageLoe(condition.getAgeLoe())
-                )
-                .fetch().stream().count();
+                );
 
-        return new PageImpl<>(results, pageable, total);
+        // - 스프링 데이터 라이브러리가 제공
+        // - count 쿼리가 생략 가능한 경우 생략해서 처리
+        //    + 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+        //    + 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
+        return PageableExecutionUtils.getPage(results, pageable, countQuery.stream()::count);
+        // return new PageImpl<>(results, pageable, total);
     }
 
     private BooleanExpression userNameEq(String userName) {
