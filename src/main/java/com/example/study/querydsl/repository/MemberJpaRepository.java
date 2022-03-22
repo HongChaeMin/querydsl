52c0ab9f312e4ed2991906a58ed8f1ed3124c299
홍채민 <coals0329@cocen.com>
2022-03-22 14:30:55 +0900

    [minHong] 순수 JPA 레파지토리와 Querydsl

---
+++ src/main/java/com/example/study/querydsl/repository/MemberJpaRepository.java
@@ -0,0 +1,52 @@
+package com.example.study.querydsl.repository;
+
+import com.example.study.querydsl.entity.Member;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.study.querydsl.entity.QMember.member;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberJpaRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Optional<Member> findById(long id) {
+        Member findMember = em.find(Member.class, id);
+        return Optional.ofNullable(findMember);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findAll_Querydsl() {
+        return queryFactory
+                .selectFrom(member)
+                .fetch();
+    }
+
+    public List<Member> findByUserName_Querydsl(String userName) {
+        return queryFactory
+                .selectFrom(member)
+                .where(member.userName.eq(userName))
+                .fetch();
+    }
+
+    public List<Member> findByUserName(String userName) {
+        return em.createQuery("select m from Member m where m.userName = :userName", Member.class)
+                .setParameter("userName", userName)
+                .getResultList();
+    }
+}
